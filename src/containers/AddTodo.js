44c7be5e440a4968778f72aed6a593ad41407b45
Julien <julien.juret@gmail.com>
2018-11-22 17:22:30 +0100

    j'ai de la chance

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -2,6 +2,32 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { addTodo } from '../actions'
 
+function randomTodo() {
+    const ACTIONS = [
+        'eat',
+        'sleep',
+        'sell',
+        'buy',
+        'destroy',
+        'throw',
+        'bury',
+    ];
+
+
+    const OBJECTS = [
+        'the banana',
+        'the dog',
+        'a fireman',
+        'a dancing guy',
+        'Station F',
+        'the coffin',
+    ];
+
+    console.log(ACTIONS[Math.round(Math.random() * ACTIONS.length)] + OBJECTS[Math.round(Math.random() * OBJECTS.length)])
+
+    return (ACTIONS[Math.round(Math.random() * ACTIONS.length)] + OBJECTS[Math.round(Math.random() * OBJECTS.length)])
+}
+
 const AddTodo = ({ dispatch }) => {
   let input
 
@@ -16,6 +42,10 @@ const AddTodo = ({ dispatch }) => {
         input.value = ''
       }}>
         <input ref={node => input = node} placeholder="new todo" /><br/>
+          <div onClick={() => {
+              dispatch(addTodo(randomTodo()))
+              input.value = ''
+          }} className="chance">J'ai de la chance</div>
       </form>
     </div>
   )
